498018b004468644185a5a0c7fdb6bcd3fef2908
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-21 18:31:17 -0400

    Update ExtraterritorialGroups.py

--- ExtraterritorialGroups.py
+++ ExtraterritorialGroups.py
@@ -1,2 +1,2 @@
 def main():
-  print("Meeting 2 industry standard references can justify doubling price.")
+  print("Meeting 2 industry standard references can justify doubling price. If they can't give 2 answers for this that aren't too good to be true, then they should not be partnered with.")
