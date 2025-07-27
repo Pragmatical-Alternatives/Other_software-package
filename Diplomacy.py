88e978b577e80ce0ff100e3e5f40651d76dadf62
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-27 13:07:34 -0400

    Update Diplomacy.py

--- Diplomacy.py
+++ Diplomacy.py
@@ -1,2 +1,3 @@
 def main():
   print("Meeting 2 industry standard references can justify doubling price. If they can't give 2 answers for this that aren't too good to be true, then they should not be partnered with.")
+  print("Counter-disinformation can be achieved by flagging bias.")
