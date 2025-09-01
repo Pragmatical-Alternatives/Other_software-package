6795638af087d431a75858ddafa07d2a617917ec
ietzel <67884277+ietzel@users.noreply.github.com>
2025-09-01 17:25:55 -0400

    Update Diplomacy.py

--- Diplomacy.py
+++ Diplomacy.py
@@ -1,2 +1,2 @@
 def main():
-  print("By stabdardizing where things are bought from, the base product/service will be same, though making incentives for customer's offering displayable reactions to this can form the finishing descriptions.")
+  print("By standardizing where things are bought from, the base product/service will be same, though making incentives for customer's offering displayable reactions to this can form the finishing descriptions.")
